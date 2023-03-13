f2c21750683ca18df496b9b263c507d9ebd26588
Murad011 <muradaydin122@gmail.com>
2023-03-13 23:42:44 +0400

    requirements.txt yenilendi

--- blog/account/api/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class NotAuthenticated(BasePermission):
-    def has_permission(self, request, view):
-        return  request.user and  request.user.is_authenticated
-
--- blog/favourite/api/pagination.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class FavouritePagination(PageNumberPagination):
-    page_size = 4
